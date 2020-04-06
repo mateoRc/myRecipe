6c15d6fe5f9a7daf6d72523ac93e9a601dc19f22
mateoLaptop <mmateo19992@gmail.com>
2020-04-06 14:53:16 +0200

    Search view, ajax loading spinner, pagination

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@
  * 
  */ 
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * 
@@ -17,23 +19,41 @@ const state = {};
 const controlSearch = async () => {
     
     // 1) get query from view
-    const query = 'pizza' // TODO
+    const query = searchView.getInput();
+    console.log(query);
     
     if (query) {
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
+        searchView.clearInput(); // clear the search field
+        searchView.clearResults(); // clear results
+        renderLoader(elements.searchRes); // render loader
 
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        // console.log(state.search.result);
+        clearLoader(); // clear the loader
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault(); // dont refresh page
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e =>{
+    const btn = e.target.closest('.btn-inline');
+    //console.log(btn);
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        //console.log(goToPage);
+    }
 });
