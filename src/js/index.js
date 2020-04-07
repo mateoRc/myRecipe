85e9e4a3e1aece337d2b7959e6e9bb60c52a2dc0
mateoLaptop <mmateo19992@gmail.com>
2020-04-07 19:31:27 +0200

    recipe model pt2

--- src/js/index.js
+++ src/js/index.js
@@ -24,8 +24,7 @@ const state = {};
 const controlSearch = async () => {
     
     // 1) get query from view
-    // const query = searchView.getInput();
-    const query = 'pizza';
+    const query = searchView.getInput();
     // console.log(query);
     
     if (query) {
@@ -56,12 +55,6 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
-// TESTING
-window.addEventListener('load', e => {
-    e.preventDefault(); // dont refresh page
-    controlSearch();
-});
-
 elements.searchResPages.addEventListener('click', e =>{
     const btn = e.target.closest('.btn-inline');
     //console.log(btn);
@@ -86,14 +79,12 @@ const controlRecipe = async () => {
 
         // Create a new recipe object
         state.recipe = new Recipe(id);
-        
-        //TESTING
-        window.r = state.recipe;
-        
+      
         try {
-            // Get recipe data
+            // Get recipe data and parse ingredients
             await state.recipe.getRecipe();
-        
+            state.recipe.parseIngredients();
+
         // calculate servings and time
         state.recipe.calcTime();
         state.recipe.calcServings();
